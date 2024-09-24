ddaf2a962b4fb2df1ec9528437e21887da71d615
Jon Williams <j.williams.softdev@gmail.com>
2024-09-24 15:36:25 +0000

    added a rules section to the top of the page

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def rules_section():
+    """
+    Displays the rules at the top of the screen.
+    """
+
+    print("Welcome to Python Checkers!")
+    print("Rules:")
+    print("1. Players alternate turns. You are 'O' and the computer is 'X'.")
+    print("2. To move, input row and column (e.g., '2 3').")
+    print("3. You can move diagonally forward. Jumping over the opponent is allowed.")
+    print("4. The goal is to capture all the opponent's pieces or block them from moving.\n")
+
+rules_section()
